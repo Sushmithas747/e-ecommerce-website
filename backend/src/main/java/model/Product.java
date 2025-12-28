d8ff4ffee73814221a274c6f13b6b520c933e725
Sushmitha S <sushmithas2190@gmail.com>
2025-12-28 17:52:32 +0530

    Create Product model (closes #4)

---
+++ backend/src/main/java/model/Product.java
@@ -0,0 +1,81 @@
+package com.sush.ecom_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private String image;
+    private Integer price;
+    private String discount;
+
+    // REQUIRED by JPA
+    public Product() {
+    }
+
+    // Constructor used by DataLoader
+    public Product(Long id, String name, String category, String image, Integer price, String discount) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.image = image;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+}
